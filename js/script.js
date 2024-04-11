743e2ba68a4f25c747fa90048f9e7c20c65c132c
Matthew West <Mwest1310@outlook.com>
2024-04-11 09:37:13 +0100

    fixed-strength-value

--- js/script.js
+++ js/script.js
@@ -11,31 +11,32 @@ jQuery(function() {
     const numbers = /\d/;
     const special = /\W/;
     //const invalid = /\s/;
-    let strength = 0;
+    
 
     $("#submit").on("click", function() {
+        let strength = 0;
         $password = $("#password").val();
         if($password.length < 8) {
             $("#advice").text("Your password must be 8+ characters long");
             return;
         }
-        if($password.match(/\s/)) {
+        if(/\s/.test($password)) {
             $("#advice").text("Your password must not include spaces");
             return;
         }
-        if($password.match(lowerCase)) {
+        if(lowerCase.test($password)) {
             strength ++;
         }
-        if($password.match(upperCase)) {
+        if(upperCase.test($password)) {
             strength ++;
         }
-        if($password.match(numbers)) {
+        if(numbers.test($password)) {
             strength ++;
         }
-        if($password.match(special)) {
+        if(special.test($password)) {
             strength ++;
         }
-
+        console.log(strength);
         switch(strength) {
             case 1:
                 $("#advice").text(strength + " : Your password is not strong enough. Try to include capital letters, numbers and special characters");
@@ -44,9 +45,11 @@ jQuery(function() {
             case 2:
                 $("#advice").text(strength + " : Your password is somewhat strong, but should be improved. Try to include capital letters, numbers and/or special characters");
                 break;
+
             case 3:
                 $("#advice").text(strength + " : Your password is strong but can still be improved. Try to include capital letters, numbers or special characters");
                 break;
+
             case 4:
                 $("#advice").text(strength + " : Your password is very strong. Congratulations!")
                 break;
